1f9783059a88d6d2d7f4bdaaedf9635caccee90a
User13 <user13@user13de-imac.local>
2019-01-11 18:58:55 +0800

    final

--- tetris/GameViewController.swift
+++ tetris/GameViewController.swift
@@ -8,43 +8,157 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
-class GameViewController: UIViewController {
 
+
+class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognizerDelegate {
+    
+    var scene: GameScene!
+    var swiftris:Swiftris!
+    var panPointReference:CGPoint?
+    
+    @IBOutlet weak var scoreLebal: UILabel!
+    
+    @IBOutlet weak var levelLebal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+        // Configure the view.
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        scene.tick = didTick
+        
+        swiftris = Swiftris()
+        swiftris.delegate = self
+        swiftris.beginGame()
+        // Present the scene.
+        skView.presentScene(scene)
+        
+    }
+    
+    @IBAction func didPressDown(_ sender: UIButton){
+        swiftris.dropShape()
+    }
+    
+    @IBAction func didPan(_ sender: UIPanGestureRecognizer) {
+        let currentPoint = sender.translation(in: self.view)
+        if let originalPoint = panPointReference {
+            
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
                 
-                // Present the scene
-                view.presentScene(scene)
+                if sender.velocity(in: self.view).x > CGFloat(0) {
+                    swiftris.moveShapeRight()
+                    panPointReference = currentPoint
+                } else {
+                    swiftris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
             }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        } else if sender.state == .began {
+            panPointReference = currentPoint
         }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
+    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
+         swiftris.rotateShape()
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+    func didTick() {
+        swiftris.letShapeFall()
+    }
+    func nextShape() {
+        let newShapes = swiftris.newShape()
+        guard let fallingShape = newShapes.fallingShape else {
+            return
+        }
+        self.scene.addPreviewShapeToScene(shape: newShapes.nextShape!) {}
+        self.scene.movePreviewShape(shape: fallingShape) {
+            
+            self.view.isUserInteractionEnabled = true
+            self.scene.startTicking()
+        }
+    }
+    
+    func gameDidBegin(swiftris: Swiftris) {
+        levelLebal.text = "\(swiftris.level)"
+        scoreLebal.text = "\(swiftris.score)"
+        scene.tickLengthMillis = TickLengthLevelOne
+        // The following is false when restarting a new game
+        if swiftris.nextShape != nil && swiftris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(shape: swiftris.nextShape!) {
+                self.nextShape()
+            }
         } else {
-            return .all
+            nextShape()
         }
     }
-
-    override var prefersStatusBarHidden: Bool {
+    
+    func gameDidEnd(swiftris: Swiftris) {
+        view.isUserInteractionEnabled = false
+        scene.stopTicking()
+        scene.playSound(sound: "Sounds/gameover.mp3")
+        scene.animateCollapsingLines(linesToRemove: swiftris.removeAllBlocks(), fallenBlocks: swiftris.removeAllBlocks()) {
+            swiftris.beginGame()
+        }
+    }
+    
+    func gameDidLevelUp(swiftris: Swiftris) {
+        levelLebal.text = "\(swiftris.level)"
+        if scene.tickLengthMillis >= 100 {
+            scene.tickLengthMillis -= 100
+        } else if scene.tickLengthMillis > 50 {
+            scene.tickLengthMillis -= 50
+        }
+        scene.playSound(sound: "Sounds/levelup.mp3")
+    }
+    
+    func gameShapeDidDrop(swiftris: Swiftris) {
+        
+        scene.stopTicking()
+        scene.redrawShape(shape: swiftris.fallingShape!) {
+            swiftris.letShapeFall()
+        }
+        scene.playSound(sound: "Sounds/drop.mp3")
+    }
+    func gameShapeDidLand(swiftris: Swiftris) {
+        scene.stopTicking()
+        self.view.isUserInteractionEnabled = false
+        
+        let removedLines = swiftris.removeCompletedLines()
+        if removedLines.linesRemoved.count > 0 {
+            self.scoreLebal.text = "\(swiftris.score)"
+            scene.animateCollapsingLines(linesToRemove: removedLines.linesRemoved, fallenBlocks:removedLines.fallenBlocks) {
+                
+                self.gameShapeDidLand(swiftris: swiftris)
+            }
+            scene.playSound(sound: "Sounds/bomb.mp3")
+        } else {
+            nextShape()
+        }
+    }
+    
+    
+    func gameShapeDidMove(swiftris: Swiftris) {
+        scene.redrawShape(shape: swiftris.fallingShape!) {}
+    }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
+    }
+    
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UISwipeGestureRecognizer {
+            if otherGestureRecognizer is UIPanGestureRecognizer {
+                return true
+            }
+        } else if gestureRecognizer is UIPanGestureRecognizer {
+            if otherGestureRecognizer is UITapGestureRecognizer {
+                return true
+            }
+        }
+        return false
     }
 }
